ac8dbf45b27e204807e67eff3d8aa0b17da5e996
devcmgm <devcmontgomery@gmail.com>
2019-12-23 15:35:59 +0000

    test add

--- class01/project01.py
+++ class01/project01.py
@@ -1,10 +1,10 @@
 # Python First Program
 # Demo some key basics
-# 
+#
 # Assignment: Change myname to your name and change the numofchildren to how many children you have.
 #
 
-myname = "Zach Lewis"
+myname = "Zach Lewis more"
 
 print("My Name is: " + myname);
 
